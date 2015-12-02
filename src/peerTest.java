2b42a605406c69ab324d36864ebb0028ed0e6fbe
malsokait <m.alsokait@gmail.com>
2015-12-02 10:11:20 -0500

    Peer tester

---
+++ src/peerTest.java
@@ -0,0 +1,12 @@
+import P2PNetwork.PeerManager;
+import P2PNetwork.Tracker;
+
+/**
+ * Created by malsokait on 2015-11-30.
+ */
+public class PeerTest {
+    public static void main(String[] args){
+        PeerManager p = new PeerManager();
+        p.joinNetwork();
+    }
+}
